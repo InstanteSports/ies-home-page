e6838509d3e96187a93ff7c14f5e50c34f83ef8d
jonwu <jonwu92@gmail.com>
2015-06-30 12:19:18 -0700

    add css to android

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function($) {
 	
 	$('.timer').countdown('2015/07/30', function(event) {
-	    $(this).html(event.strftime('%-D DAYS %H:%M:%S'));
+		$(this).html(event.strftime('%-D DAYS'));
+	  });
+	$('.hour').countdown('2015/07/30', function(event) {
+		$(this).html(event.strftime('%H : %M : %S'));
 	  });
 });
