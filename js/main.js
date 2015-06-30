381e34920974868eba7dff7c674053c20b4fcfea
jonwu <jonwu92@gmail.com>
2015-06-30 12:47:24 -0700

    change date

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ jQuery(document).ready(function($) {
 	$(".content").fadeIn('slow', function() {
 		
 	});
-	$('.timer').countdown('2015/07/30', function(event) {
+	$('.timer').countdown('2015/07/25', function(event) {
 		$(this).html(event.strftime('%-D DAYS'));
 	  });
-	$('.hour').countdown('2015/07/30', function(event) {
+	$('.hour').countdown('2015/07/25', function(event) {
 		$(this).html(event.strftime('%H : %M : %S'));
 	  });
 });
