d1f9c5fa6af4664f7ce8ae8e1e07c6c22cf4ebeb
jonzlin95 <jonzlin95@gmail.com>
2015-10-25 01:53:43 -0700

    Round 1 attempt at link texting hooked with server

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,4 +20,23 @@ $(document).ready(function(){
 	$('.cover-livetext-input').intlTelInput();
 	$('.cover-livetext-input').intlTelInput("loadUtils", "assets/js/intlTelInput/js/utils.js");
 
+	var telInput = $('.cover-livetext-input');
+
+
+    $('.cover-livetext-submit').click(function() {
+        if ($.trim(telInput.val())) {
+            if (telInput.intlTelInput("isValidNumber")) {
+                $.post("http://instant-esports-static.herokuapp.com/text_download/",
+                    {"phone_number": telInput.intlTelInput("getNumber")}
+                );
+            } else {
+              telInput.addClass("error");
+            }
+        }
+    })
+
+    telInput.keydown(function() {
+      telInput.removeClass("error");
+    });
+
 });
